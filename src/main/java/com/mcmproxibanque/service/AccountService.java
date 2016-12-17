65ecb6ebe7991f3d51361662f751b0ada898f79b
Mathieu Langlais <mathieu.langlais@gmail.com>
2016-12-17 10:21:51 +0100

    changement du pom

--- src/main/java/com/mcmproxibanque/service/AccountService.java
+++ src/main/java/com/mcmproxibanque/service/AccountService.java
@@ -1,5 +1,7 @@
 package com.mcmproxibanque.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -7,9 +9,39 @@ import com.mcmproxibanque.dao.interfaces.IAccountDao;
 import com.mcmproxibanque.model.Account;
 
 @Component
-public class AccountService extends ServiceImpl<Account> implements IService<Account> {
+public class AccountService implements IService<Account> {
 	
 	@Autowired
-	IAccountDao accountDaoImpl;	
+	IAccountDao accountDaoImpl;
+
+	@Override
+	public void persist(Account e) throws Exception {
+		accountDaoImpl.persist(e);
+		
+	}
+
+	@Override
+	public void merge(Account e) throws Exception {
+		accountDaoImpl.merge(e);
+		
+	}
+
+	@Override
+	public void remove(Object id) throws Exception {
+		accountDaoImpl.remove(id);
+		
+	}
+
+	@Override
+	public Account findById(Object id) throws Exception {
+		Account account = accountDaoImpl.findById(id);
+		return account;
+	}
+
+	@Override
+	public List<Account> findAll() throws Exception {
+		List<Account> listAccount = accountDaoImpl.findAll();
+		return listAccount;
+	}	
 
 }
--- src/main/java/com/mcmproxibanque/service/AdvisorService.java
+++ src/main/java/com/mcmproxibanque/service/AdvisorService.java
@@ -1,10 +1,46 @@
 package com.mcmproxibanque.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.mcmproxibanque.model.Advisor;
 
 @Component
-public class AdvisorService extends ServiceImpl<Advisor> {
+public class AdvisorService implements IService<Advisor> {
+
+	@Autowired
+	
+	
+	@Override
+	public void persist(Advisor e) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void merge(Advisor e) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(Object id) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Advisor findById(Object id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Advisor> findAll() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
